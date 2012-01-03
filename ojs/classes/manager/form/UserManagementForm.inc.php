mcrider/ojs

*7000* Interest overhaul

classes/manager/form/UserManagementForm.inc.php
@@ -140,19 +140,12 @@ class UserManagementForm extends Form {
 	 * Initialize form data from current user profile.
 	 */
 	function initData(&$args, &$request) {
-		$interestDao =& DAORegistry::getDAO('InterestDAO');
 		if (isset($this->userId)) {
 			$userDao =& DAORegistry::getDAO('UserDAO');
 			$user =& $userDao->getUser($this->userId);
 
-			// Get all available interests to populate the autocomplete with
-			if ($interestDao->getAllUniqueInterests()) {
-				$existingInterests = $interestDao->getAllUniqueInterests();
-			} else $existingInterests = null;
-			// Get the user's current set of interests
-			if ($interestDao->getInterests($user->getId())) {
-				$currentInterests = $interestDao->getInterests($user->getId());
-			} else $currentInterests = null;
+			import('lib.pkp.classes.user.InterestManager');
+			$interestManager = new InterestManager();
 
 			if ($user != null) {
 				$this->_data = array(
@@ -173,8 +166,8 @@ class UserManagementForm extends Form {
 					'mailingAddress' => $user->getMailingAddress(),
 					'country' => $user->getCountry(),
 					'biography' => $user->getBiography(null), // Localized
-					'existingInterests' => $existingInterests,
-					'interestsKeywords' => $currentInterests,
+					'interestsKeywords' => $interestManager->getInterestsForUser($user),
+					'interestsTextOnly' => $interestManager->getInterestsForUserAsFormattedString($user),
 					'gossip' => $user->getGossip(null), // Localized
 					'userLocales' => $user->getLocales()
 				);
@@ -218,8 +211,8 @@ class UserManagementForm extends Form {
 			'mailingAddress',
 			'country',
 			'biography',
-			'interestsKeywords',
-			'interests',
+			'interestsTextOnly',
+			'keywords',
 			'gossip',
 			'userLocales',
 			'generatePassword',
@@ -239,10 +232,10 @@ class UserManagementForm extends Form {
 			$this->setData('username', strtolower($this->getData('username')));
 		}
 
-		$interests = $this->getData('interestsKeywords');
-		if ($interests != null && is_array($interests)) {
+		$keywords = $this->getData('keywords');
+		if ($keywords != null && is_array($keywords['interests'])) {
 			// The interests are coming in encoded -- Decode them for DB storage
-			$this->setData('interestsKeywords', array_map('urldecode', $interests));
+			$this->setData('interestsKeywords', array_map('urldecode', $keywords['interests']));
 		}
 	}
 
@@ -370,9 +363,11 @@ class UserManagementForm extends Form {
 			}
 		}
 
+		// Insert the user interests
+		$interests = $this->getData('interestsKeywords') ? $this->getData('interestsKeywords') : $this->getData('interestsTextOnly');
 		import('lib.pkp.classes.user.InterestManager');
 		$interestManager = new InterestManager();
-		$interestManager->insertInterests($userId, $this->getData('interestsKeywords'), $this->getData('interests'));
+		$interestManager->setInterestsForUser($user, $interests);
 	}
 }
 

classes/user/form/ProfileForm.inc.php
@@ -162,16 +162,9 @@ class ProfileForm extends Form {
 	 */
 	function initData(&$args, &$request) {
 		$user =& $request->getUser();
-		$interestDao =& DAORegistry::getDAO('InterestDAO');
 
-		// Get all available interests to populate the autocomplete with
-		if ($interestDao->getAllUniqueInterests()) {
-			$existingInterests = $interestDao->getAllUniqueInterests();
-		} else $existingInterests = null;
-		// Get the user's current set of interests
-		if ($interestDao->getInterests($user->getId())) {
-			$currentInterests = $interestDao->getInterests($user->getId());
-		} else $currentInterests = null;
+		import('lib.pkp.classes.user.InterestManager');
+		$interestManager = new InterestManager();
 
 		$this->_data = array(
 			'salutation' => $user->getSalutation(),
@@ -193,8 +186,8 @@ class ProfileForm extends Form {
 			'isAuthor' => Validation::isAuthor(),
 			'isReader' => Validation::isReader(),
 			'isReviewer' => Validation::isReviewer(),
-			'existingInterests' => $existingInterests,
-			'interestsKeywords' => $currentInterests
+			'interestsKeywords' => $interestManager->getInterestsForUser($user),
+			'interestsTextOnly' => $interestManager->getInterestsForUserAsFormattedString($user),
 		);
 	}
 
@@ -218,8 +211,8 @@ class ProfileForm extends Form {
 			'mailingAddress',
 			'country',
 			'biography',
-			'interests',
-			'interestsKeywords',
+			'keywords',
+			'interestsTextOnly',
 			'userLocales',
 			'readerRole',
 			'authorRole',
@@ -230,10 +223,10 @@ class ProfileForm extends Form {
 			$this->setData('userLocales', array());
 		}
 
-		$interests = $this->getData('interestsKeywords');
-		if ($interests != null && is_array($interests)) {
+		$keywords = $this->getData('keywords');
+		if ($keywords != null && is_array($keywords['interests'])) {
 			// The interests are coming in encoded -- Decode them for DB storage
-			$this->setData('interestsKeywords', array_map('urldecode', $interests));
+			$this->setData('interestsKeywords', array_map('urldecode', $keywords['interests']));
 		}
 	}
 
@@ -258,13 +251,12 @@ class ProfileForm extends Form {
 		$user->setMailingAddress($this->getData('mailingAddress'));
 		$user->setCountry($this->getData('country'));
 		$user->setBiography($this->getData('biography'), null); // Localized
-		$userId = $user->getId();
 
 		// Insert the user interests
+		$interests = $this->getData('interestsKeywords') ? $this->getData('interestsKeywords') : $this->getData('interestsTextOnly');
 		import('lib.pkp.classes.user.InterestManager');
 		$interestManager = new InterestManager();
-		$interestManager->insertInterests($userId, $this->getData('interestsKeywords'), $this->getData('interests'));
-
+		$interestManager->setInterestsForUser($user, $interests);
 
 		$site =& Request::getSite();
 		$availableLocales = $site->getSupportedLocales();

templates/manager/people/userProfileForm.tpl
@@ -14,22 +14,6 @@
 {include file="common/header.tpl"}
 {/strip}
 
-{literal}
-<script type="text/javascript">
-	$(document).ready(function(){
-		$("#interestsTextOnly").hide();
-		$("#interests").tagit({
-			{/literal}{if $existingInterests}{literal}
-			// This is the list of interests in the system used to populate the autocomplete
-			availableTags: [{/literal}{foreach name=existingInterests from=$existingInterests item=interest}"{$interest|escape|escape:'javascript'}"{if !$smarty.foreach.existingInterests.last}, {/if}{/foreach}{literal}],{/literal}{/if}
-			// This is the list of the user's interests that have already been saved
-			{if $interestsKeywords}{literal}currentTags: [{/literal}{foreach name=currentInterests from=$interestsKeywords item=interest}"{$interest|escape|escape:'javascript'}"{if !$smarty.foreach.currentInterests.last}, {/if}{/foreach}{literal}]{/literal}
-			{else}{literal}currentTags: []{/literal}{/if}{literal}
-		});
-	});
-</script>
-{/literal}
-
 {if not $userId}
 {assign var="passwordRequired" value="true"}
 
@@ -234,10 +218,8 @@
 	</tr>
 	<tr valign="top">
 		<td class="label">{fieldLabel for="interests" key="user.interests"}</td>
-		<td class="value"><ul id="interests"><li></li></ul><span class="interestDescription">{fieldLabel for="interests" key="user.interests.description"}</span><br />
-			<textarea name="interests" id="interestsTextOnly" rows="5" cols="40" class="textArea">
-				{foreach name=currentInterests from=$interestsKeywords item=interest}{$interest|escape}{if !$smarty.foreach.currentInterests.last}, {/if}{/foreach}
-			</textarea>
+		<td class="value">
+			{include file="user/interestsInput.tpl" interestKeywords=$interestsKeywords interestsTextOnly=$interestsTextOnly}
 		</td>
 	</tr>
 	<tr valign="top">

templates/sectionEditor/createReviewerForm.tpl
@@ -13,22 +13,6 @@
 {include file="common/header.tpl"}
 {/strip}
 
-{literal}
-<script type="text/javascript">
-	$(document).ready(function(){
-		$("#interestsTextOnly").hide();
-		$("#interests").tagit({
-			{/literal}{if $existingInterests}{literal}
-			// This is the list of interests in the system used to populate the autocomplete
-			availableTags: [{/literal}{foreach name=existingInterests from=$existingInterests item=interest}"{$interest|escape|escape:'javascript'}"{if !$smarty.foreach.existingInterests.last}, {/if}{/foreach}{literal}],{/literal}{/if}
-			// This is the list of the user's interests that have already been saved
-			{if $interestsKeywords}{literal}currentTags: [{/literal}{foreach name=currentInterests from=$interestsKeywords item=interest}"{$interest|escape|escape:'javascript'}"{if !$smarty.foreach.currentInterests.last}, {/if}{/foreach}{literal}]{/literal}
-			{else}{literal}currentTags: []{/literal}{/if}{literal}
-		});
-	});
-</script>
-{/literal}
-
 <form method="post" id="reviewerForm" action="{url op="createReviewer" path=$articleId|to_array:"create"}">
 
 {include file="common/formErrors.tpl"}
@@ -134,7 +118,9 @@
 	</tr>
 	<tr valign="top">
 		<td class="label">{fieldLabel for="interests" key="user.interests"}</td>
-		<td class="value"><ul id="interests"><li></li></ul><span class="interestDescription">{fieldLabel for="interests" key="user.interests.description"}</span><br /><textarea name="interests" id="interestsTextOnly" rows="5" cols="40" class="textArea">{foreach name=currentInterests from=$interestsKeywords item=interest}{$interest|escape}{if !$smarty.foreach.currentInterests.last}, {/if}{/foreach}</textarea></td>
+		<td class="value">
+			{include file="user/interestsInput.tpl" interestKeywords=$interestsKeywords interestsTextOnly=$interestsTextOnly}
+		</td>
 	</tr>
 	<tr valign="top">
 		<td class="label">{fieldLabel name="gossip" key="user.gossip"}</td>

templates/user/profile.tpl
@@ -14,23 +14,6 @@
 {include file="common/header.tpl"}
 {/strip}
 
-{literal}
-
-<script type="text/javascript">
-	$(document).ready(function(){
-		$("#interestsTextOnly").hide();
-		$("#interests").tagit({
-			{/literal}{if $existingInterests}{literal}
-			// This is the list of interests in the system used to populate the autocomplete
-			availableTags: [{/literal}{foreach name=existingInterests from=$existingInterests item=interest}"{$interest|escape|escape:'javascript'}"{if !$smarty.foreach.existingInterests.last}, {/if}{/foreach}{literal}],{/literal}{/if}
-			// This is the list of the user's interests that have already been saved
-			{if $interestsKeywords}{literal}currentTags: [{/literal}{foreach name=currentInterests from=$interestsKeywords item=interest}"{$interest|escape|escape:'javascript'}"{if !$smarty.foreach.currentInterests.last}, {/if}{/foreach}{literal}]{/literal}
-			{else}{literal}currentTags: []{/literal}{/if}{literal}
-		});
-	});
-</script>
-{/literal}
-
 <form id="profile" method="post" action="{url op="saveProfile"}" enctype="multipart/form-data">
 
 {include file="common/formErrors.tpl"}
@@ -139,12 +122,7 @@
 <tr valign="top">
 	<td class="label">{fieldLabel name="interests" key="user.interests"}</td>
 	<td class="value">
-		<!-- The container which will be processed by tag-it.js as the interests widget -->
-		<ul id="interests"><li></li></ul><span class="interestDescription">{fieldLabel for="interests" key="user.interests.description"}</span><br />
-		<!-- If Javascript is disabled, this field will be visible -->
-		<textarea name="interests" id="interestsTextOnly" rows="5" cols="40" class="textArea">
-			{foreach name=currentInterests from=$interestsKeywords item=interest}{$interest|escape}{if !$smarty.foreach.currentInterests.last}, {/if}{/foreach}
-		</textarea>
+		{include file="user/interestsInput.tpl" interestKeywords=$interestsKeywords interestsTextOnly=$interestsTextOnly}
 	</td>
 </tr>
 {/if}

templates/user/register.tpl
@@ -13,22 +13,6 @@
 {include file="common/header.tpl"}
 {/strip}
 
-{literal}
-<script type="text/javascript">
-	$(document).ready(function(){
-		$("#interestsTextOnly").hide();
-		$("#interests").tagit({
-			{/literal}{if $existingInterests}{literal}
-			// This is the list of interests in the system used to populate the autocomplete
-			availableTags: [{/literal}{foreach name=existingInterests from=$existingInterests item=interest}"{$interest|escape|escape:'javascript'}"{if !$smarty.foreach.existingInterests.last}, {/if}{/foreach}{literal}],{/literal}{/if}
-			// This is the list of the user's interests that have already been saved
-			{if $interestsKeywords}{literal}currentTags: [{/literal}{foreach name=currentInterests from=$interestsKeywords item=interest}"{$interest|escape|escape:'javascript'}"{if !$smarty.foreach.currentInterests.last}, {/if}{/foreach}{literal}]{/literal}
-			{else}{literal}currentTags: []{/literal}{/if}{literal}
-		});
-	});
-</script>
-{/literal}
-
 <form id="register" method="post" action="{url op="registerUser"}">
 
 <p>{translate key="user.register.completeForm"}</p>
@@ -232,10 +216,7 @@
 		{if $allowRegReviewer || $allowRegReviewer === null}<input type="checkbox" name="registerAsReviewer" id="registerAsReviewer" value="1"{if $registerAsReviewer} checked="checked"{/if} /> <label for="registerAsReviewer">{translate key="user.role.reviewer"}</label>: {if $existingUser}{translate key="user.register.reviewerDescriptionNoInterests"}{else}{translate key="user.register.reviewerDescription"}{/if}
 		<br /><div id="reviewerInterestsContainer" style="margin-left:25px;">
 			<label class="desc">{translate key="user.register.reviewerInterests"}</label>
-			<ul id="interests"><li></li></ul><span class="interestDescription">{fieldLabel for="interests" key="user.interests.description"}</span>
-			<textarea name="interests" id="interestsTextOnly" rows="5" cols="40" class="textArea">
-					{foreach name=currentInterests from=$interestsKeywords item=interest}{$interest|escape}{if !$smarty.foreach.currentInterests.last}, {/if}{/foreach}
-			</textarea>
+			{include file="user/interestsInput.tpl" interestKeywords=$interestsKeywords interestsTextOnly=$interestsTextOnly}
 		</div>
 		</td>
 		{/if}

jardakotesovec/pkp-mono

ojs/classes/manager/form/UserManagementForm.inc.php
@@ -140,19 +140,12 @@ class UserManagementForm extends Form {
 	 * Initialize form data from current user profile.
 	 */
 	function initData(&$args, &$request) {
-		$interestDao =& DAORegistry::getDAO('InterestDAO');
 		if (isset($this->userId)) {
 			$userDao =& DAORegistry::getDAO('UserDAO');
 			$user =& $userDao->getUser($this->userId);
 
-			// Get all available interests to populate the autocomplete with
-			if ($interestDao->getAllUniqueInterests()) {
-				$existingInterests = $interestDao->getAllUniqueInterests();
-			} else $existingInterests = null;
-			// Get the user's current set of interests
-			if ($interestDao->getInterests($user->getId())) {
-				$currentInterests = $interestDao->getInterests($user->getId());
-			} else $currentInterests = null;
+			import('lib.pkp.classes.user.InterestManager');
+			$interestManager = new InterestManager();
 
 			if ($user != null) {
 				$this->_data = array(
@@ -173,8 +166,8 @@ class UserManagementForm extends Form {
 					'mailingAddress' => $user->getMailingAddress(),
 					'country' => $user->getCountry(),
 					'biography' => $user->getBiography(null), // Localized
-					'existingInterests' => $existingInterests,
-					'interestsKeywords' => $currentInterests,
+					'interestsKeywords' => $interestManager->getInterestsForUser($user),
+					'interestsTextOnly' => $interestManager->getInterestsString($user),
 					'gossip' => $user->getGossip(null), // Localized
 					'userLocales' => $user->getLocales()
 				);
@@ -218,8 +211,8 @@ class UserManagementForm extends Form {
 			'mailingAddress',
 			'country',
 			'biography',
-			'interestsKeywords',
-			'interests',
+			'interestsTextOnly',
+			'keywords',
 			'gossip',
 			'userLocales',
 			'generatePassword',
@@ -239,10 +232,10 @@ class UserManagementForm extends Form {
 			$this->setData('username', strtolower($this->getData('username')));
 		}
 
-		$interests = $this->getData('interestsKeywords');
-		if ($interests != null && is_array($interests)) {
+		$keywords = $this->getData('keywords');
+		if ($keywords != null && is_array($keywords['interests'])) {
 			// The interests are coming in encoded -- Decode them for DB storage
-			$this->setData('interestsKeywords', array_map('urldecode', $interests));
+			$this->setData('interestsKeywords', array_map('urldecode', $keywords['interests']));
 		}
 	}
 
@@ -370,9 +363,11 @@ class UserManagementForm extends Form {
 			}
 		}
 
+		// Insert the user interests
+		$interests = $this->getData('interestsKeywords') ? $this->getData('interestsKeywords') : $this->getData('interestsTextOnly');
 		import('lib.pkp.classes.user.InterestManager');
 		$interestManager = new InterestManager();
-		$interestManager->insertInterests($userId, $this->getData('interestsKeywords'), $this->getData('interests'));
+		$interestManager->setInterestsForUser($user, $interests);
 	}
 }
 

ojs/classes/user/form/ProfileForm.inc.php
@@ -162,16 +162,9 @@ class ProfileForm extends Form {
 	 */
 	function initData(&$args, &$request) {
 		$user =& $request->getUser();
-		$interestDao =& DAORegistry::getDAO('InterestDAO');
 
-		// Get all available interests to populate the autocomplete with
-		if ($interestDao->getAllUniqueInterests()) {
-			$existingInterests = $interestDao->getAllUniqueInterests();
-		} else $existingInterests = null;
-		// Get the user's current set of interests
-		if ($interestDao->getInterests($user->getId())) {
-			$currentInterests = $interestDao->getInterests($user->getId());
-		} else $currentInterests = null;
+		import('lib.pkp.classes.user.InterestManager');
+		$interestManager = new InterestManager();
 
 		$this->_data = array(
 			'salutation' => $user->getSalutation(),
@@ -193,8 +186,8 @@ class ProfileForm extends Form {
 			'isAuthor' => Validation::isAuthor(),
 			'isReader' => Validation::isReader(),
 			'isReviewer' => Validation::isReviewer(),
-			'existingInterests' => $existingInterests,
-			'interestsKeywords' => $currentInterests
+			'interestsKeywords' => $interestManager->getInterestsForUser($user),
+			'interestsTextOnly' => $interestManager->getInterestsString($user),
 		);
 	}
 
@@ -218,8 +211,8 @@ class ProfileForm extends Form {
 			'mailingAddress',
 			'country',
 			'biography',
-			'interests',
-			'interestsKeywords',
+			'keywords',
+			'interestsTextOnly',
 			'userLocales',
 			'readerRole',
 			'authorRole',
@@ -230,10 +223,10 @@ class ProfileForm extends Form {
 			$this->setData('userLocales', array());
 		}
 
-		$interests = $this->getData('interestsKeywords');
-		if ($interests != null && is_array($interests)) {
+		$keywords = $this->getData('keywords');
+		if ($keywords != null && is_array($keywords['interests'])) {
 			// The interests are coming in encoded -- Decode them for DB storage
-			$this->setData('interestsKeywords', array_map('urldecode', $interests));
+			$this->setData('interestsKeywords', array_map('urldecode', $keywords['interests']));
 		}
 	}
 
@@ -258,13 +251,12 @@ class ProfileForm extends Form {
 		$user->setMailingAddress($this->getData('mailingAddress'));
 		$user->setCountry($this->getData('country'));
 		$user->setBiography($this->getData('biography'), null); // Localized
-		$userId = $user->getId();
 
 		// Insert the user interests
+		$interests = $this->getData('interestsKeywords') ? $this->getData('interestsKeywords') : $this->getData('interestsTextOnly');
 		import('lib.pkp.classes.user.InterestManager');
 		$interestManager = new InterestManager();
-		$interestManager->insertInterests($userId, $this->getData('interestsKeywords'), $this->getData('interests'));
-
+		$interestManager->setInterestsForUser($user, $interests);
 
 		$site =& Request::getSite();
 		$availableLocales = $site->getSupportedLocales();

ojs/templates/manager/people/userProfileForm.tpl
@@ -14,22 +14,6 @@
 {include file="common/header.tpl"}
 {/strip}
 
-{literal}
-<script type="text/javascript">
-	$(document).ready(function(){
-		$("#interestsTextOnly").hide();
-		$("#interests").tagit({
-			{/literal}{if $existingInterests}{literal}
-			// This is the list of interests in the system used to populate the autocomplete
-			availableTags: [{/literal}{foreach name=existingInterests from=$existingInterests item=interest}"{$interest|escape|escape:'javascript'}"{if !$smarty.foreach.existingInterests.last}, {/if}{/foreach}{literal}],{/literal}{/if}
-			// This is the list of the user's interests that have already been saved
-			{if $interestsKeywords}{literal}currentTags: [{/literal}{foreach name=currentInterests from=$interestsKeywords item=interest}"{$interest|escape|escape:'javascript'}"{if !$smarty.foreach.currentInterests.last}, {/if}{/foreach}{literal}]{/literal}
-			{else}{literal}currentTags: []{/literal}{/if}{literal}
-		});
-	});
-</script>
-{/literal}
-
 {if not $userId}
 {assign var="passwordRequired" value="true"}
 
@@ -234,10 +218,8 @@
 	</tr>
 	<tr valign="top">
 		<td class="label">{fieldLabel for="interests" key="user.interests"}</td>
-		<td class="value"><ul id="interests"><li></li></ul><span class="interestDescription">{fieldLabel for="interests" key="user.interests.description"}</span><br />
-			<textarea name="interests" id="interestsTextOnly" rows="5" cols="40" class="textArea">
-				{foreach name=currentInterests from=$interestsKeywords item=interest}{$interest|escape}{if !$smarty.foreach.currentInterests.last}, {/if}{/foreach}
-			</textarea>
+		<td class="value">
+			{include file="form/interestsInput.tpl" FBV_interestKeywords=$interestsKeywords FBV_interestsTextOnly=$interestsTextOnly}
 		</td>
 	</tr>
 	<tr valign="top">

ojs/templates/sectionEditor/createReviewerForm.tpl
@@ -13,22 +13,6 @@
 {include file="common/header.tpl"}
 {/strip}
 
-{literal}
-<script type="text/javascript">
-	$(document).ready(function(){
-		$("#interestsTextOnly").hide();
-		$("#interests").tagit({
-			{/literal}{if $existingInterests}{literal}
-			// This is the list of interests in the system used to populate the autocomplete
-			availableTags: [{/literal}{foreach name=existingInterests from=$existingInterests item=interest}"{$interest|escape|escape:'javascript'}"{if !$smarty.foreach.existingInterests.last}, {/if}{/foreach}{literal}],{/literal}{/if}
-			// This is the list of the user's interests that have already been saved
-			{if $interestsKeywords}{literal}currentTags: [{/literal}{foreach name=currentInterests from=$interestsKeywords item=interest}"{$interest|escape|escape:'javascript'}"{if !$smarty.foreach.currentInterests.last}, {/if}{/foreach}{literal}]{/literal}
-			{else}{literal}currentTags: []{/literal}{/if}{literal}
-		});
-	});
-</script>
-{/literal}
-
 <form method="post" id="reviewerForm" action="{url op="createReviewer" path=$articleId|to_array:"create"}">
 
 {include file="common/formErrors.tpl"}
@@ -134,7 +118,9 @@
 	</tr>
 	<tr valign="top">
 		<td class="label">{fieldLabel for="interests" key="user.interests"}</td>
-		<td class="value"><ul id="interests"><li></li></ul><span class="interestDescription">{fieldLabel for="interests" key="user.interests.description"}</span><br /><textarea name="interests" id="interestsTextOnly" rows="5" cols="40" class="textArea">{foreach name=currentInterests from=$interestsKeywords item=interest}{$interest|escape}{if !$smarty.foreach.currentInterests.last}, {/if}{/foreach}</textarea></td>
+		<td class="value">
+			{include file="form/interestsInput.tpl" FBV_interestKeywords=$interestsKeywords FBV_interestsTextOnly=$interestsTextOnly}
+		</td>
 	</tr>
 	<tr valign="top">
 		<td class="label">{fieldLabel name="gossip" key="user.gossip"}</td>

ojs/templates/user/profile.tpl
@@ -14,23 +14,6 @@
 {include file="common/header.tpl"}
 {/strip}
 
-{literal}
-
-<script type="text/javascript">
-	$(document).ready(function(){
-		$("#interestsTextOnly").hide();
-		$("#interests").tagit({
-			{/literal}{if $existingInterests}{literal}
-			// This is the list of interests in the system used to populate the autocomplete
-			availableTags: [{/literal}{foreach name=existingInterests from=$existingInterests item=interest}"{$interest|escape|escape:'javascript'}"{if !$smarty.foreach.existingInterests.last}, {/if}{/foreach}{literal}],{/literal}{/if}
-			// This is the list of the user's interests that have already been saved
-			{if $interestsKeywords}{literal}currentTags: [{/literal}{foreach name=currentInterests from=$interestsKeywords item=interest}"{$interest|escape|escape:'javascript'}"{if !$smarty.foreach.currentInterests.last}, {/if}{/foreach}{literal}]{/literal}
-			{else}{literal}currentTags: []{/literal}{/if}{literal}
-		});
-	});
-</script>
-{/literal}
-
 <form id="profile" method="post" action="{url op="saveProfile"}" enctype="multipart/form-data">
 
 {include file="common/formErrors.tpl"}
@@ -139,12 +122,7 @@
 <tr valign="top">
 	<td class="label">{fieldLabel name="interests" key="user.interests"}</td>
 	<td class="value">
-		<!-- The container which will be processed by tag-it.js as the interests widget -->
-		<ul id="interests"><li></li></ul><span class="interestDescription">{fieldLabel for="interests" key="user.interests.description"}</span><br />
-		<!-- If Javascript is disabled, this field will be visible -->
-		<textarea name="interests" id="interestsTextOnly" rows="5" cols="40" class="textArea">
-			{foreach name=currentInterests from=$interestsKeywords item=interest}{$interest|escape}{if !$smarty.foreach.currentInterests.last}, {/if}{/foreach}
-		</textarea>
+		{include file="form/interestsInput.tpl" FBV_interestKeywords=$interestsKeywords FBV_interestsTextOnly=$interestsTextOnly}
 	</td>
 </tr>
 {/if}

ojs/templates/user/register.tpl
@@ -13,22 +13,6 @@
 {include file="common/header.tpl"}
 {/strip}
 
-{literal}
-<script type="text/javascript">
-	$(document).ready(function(){
-		$("#interestsTextOnly").hide();
-		$("#interests").tagit({
-			{/literal}{if $existingInterests}{literal}
-			// This is the list of interests in the system used to populate the autocomplete
-			availableTags: [{/literal}{foreach name=existingInterests from=$existingInterests item=interest}"{$interest|escape|escape:'javascript'}"{if !$smarty.foreach.existingInterests.last}, {/if}{/foreach}{literal}],{/literal}{/if}
-			// This is the list of the user's interests that have already been saved
-			{if $interestsKeywords}{literal}currentTags: [{/literal}{foreach name=currentInterests from=$interestsKeywords item=interest}"{$interest|escape|escape:'javascript'}"{if !$smarty.foreach.currentInterests.last}, {/if}{/foreach}{literal}]{/literal}
-			{else}{literal}currentTags: []{/literal}{/if}{literal}
-		});
-	});
-</script>
-{/literal}
-
 <form id="register" method="post" action="{url op="registerUser"}">
 
 <p>{translate key="user.register.completeForm"}</p>
@@ -232,10 +216,7 @@
 		{if $allowRegReviewer || $allowRegReviewer === null}<input type="checkbox" name="registerAsReviewer" id="registerAsReviewer" value="1"{if $registerAsReviewer} checked="checked"{/if} /> <label for="registerAsReviewer">{translate key="user.role.reviewer"}</label>: {if $existingUser}{translate key="user.register.reviewerDescriptionNoInterests"}{else}{translate key="user.register.reviewerDescription"}{/if}
 		<br /><div id="reviewerInterestsContainer" style="margin-left:25px;">
 			<label class="desc">{translate key="user.register.reviewerInterests"}</label>
-			<ul id="interests"><li></li></ul><span class="interestDescription">{fieldLabel for="interests" key="user.interests.description"}</span>
-			<textarea name="interests" id="interestsTextOnly" rows="5" cols="40" class="textArea">
-					{foreach name=currentInterests from=$interestsKeywords item=interest}{$interest|escape}{if !$smarty.foreach.currentInterests.last}, {/if}{/foreach}
-			</textarea>
+			{include file="form/interestsInput.tpl" FBV_interestKeywords=$interestsKeywords FBV_interestsTextOnly=$interestsTextOnly}
 		</div>
 		</td>
 		{/if}