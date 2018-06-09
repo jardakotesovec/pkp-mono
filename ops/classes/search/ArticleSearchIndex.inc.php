pkp/ojs

Modifications for multilingual author's fields fistName, lastName, middleName

classes/search/ArticleSearchIndex.inc.php
@@ -46,9 +46,18 @@ class ArticleSearchIndex extends SubmissionSearchIndex {
 			$authors = $article->getAuthors();
 			for ($i=0, $count=count($authors); $i < $count; $i++) {
 				$author = $authors[$i];
-				array_push($authorText, $author->getFirstName());
-				array_push($authorText, $author->getMiddleName());
-				array_push($authorText, $author->getLastName());
+				$fistNames = $author->getFirstName(null);
+				if (is_array($firstNames)) foreach ($firstNames as $firstName) { // Localized
+                                        array_push($authorText, $firstName);
+                                }
+				$middleNames = $author->getMiddleName(null);
+				if (is_array($middleNames)) foreach ($middleNames as $middleName) { // Localized
+                                        array_push($authorText, $middleName);
+                                }
+				$lastNames = $author->getLastName(null);
+				if (is_array($lastNames)) foreach ($lastNames as $lastName) { // Localized
+                                        array_push($authorText, $lastName);
+                                }
 				$affiliations = $author->getAffiliation(null);
 				if (is_array($affiliations)) foreach ($affiliations as $affiliation) { // Localized
 					array_push($authorText, $affiliation);

jardakotesovec/pkp-mono

ops/classes/search/ArticleSearchIndex.inc.php
@@ -46,9 +46,18 @@ class ArticleSearchIndex extends SubmissionSearchIndex {
 			$authors = $article->getAuthors();
 			for ($i=0, $count=count($authors); $i < $count; $i++) {
 				$author = $authors[$i];
-				array_push($authorText, $author->getFirstName());
-				array_push($authorText, $author->getMiddleName());
-				array_push($authorText, $author->getLastName());
+				$firstNames = $author->getFirstName(null);
+				if (is_array($firstNames)) foreach ($firstNames as $firstName) { // Localized
+                                        array_push($authorText, $firstName);
+                                }
+				$middleNames = $author->getMiddleName(null);
+				if (is_array($middleNames)) foreach ($middleNames as $middleName) { // Localized
+                                        array_push($authorText, $middleName);
+                                }
+				$lastNames = $author->getLastName(null);
+				if (is_array($lastNames)) foreach ($lastNames as $lastName) { // Localized
+                                        array_push($authorText, $lastName);
+                                }
 				$affiliations = $author->getAffiliation(null);
 				if (is_array($affiliations)) foreach ($affiliations as $affiliation) { // Localized
 					array_push($authorText, $affiliation);