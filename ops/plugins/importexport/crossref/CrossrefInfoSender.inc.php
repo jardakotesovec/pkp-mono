bozana/ojs

new crossref deposit api

plugins/importexport/crossref/CrossrefInfoSender.inc.php
@@ -61,22 +61,9 @@ class CrossrefInfoSender extends ScheduledTask {
 			if ($doiPubIdPlugin->getSetting($journal->getId(), 'enableSubmissionDoi')) {
 				// Get unregistered articles
 				$unregisteredArticles = $plugin->getUnregisteredArticles($journal);
-				// Update the status and construct an array of the articles to be deposited
-				$articlesToBeDeposited = $this->_getObjectsToBeDeposited($unregisteredArticles, $journal, $notify);
-				// If there are articles to be deposited and we want automatic deposits
-				if (count($articlesToBeDeposited) && $plugin->getSetting($journal->getId(), 'automaticRegistration')) {
-					$this->_registerObjects($articlesToBeDeposited, 'article=>crossref-xml', $journal, 'articles');
-				}
-			}
-
-			// Notify journal managers if there is a new failed DOI status
-			if ($notify) {
-				$roleDao = DAORegistry::getDAO('RoleDAO');
-				$journalManagers = $roleDao->getUsersByRoleId(ROLE_ID_MANAGER, $journal->getId());
-				import('classes.notification.NotificationManager');
-				$notificationManager = new NotificationManager();
-				while ($journalManager = $journalManagers->next()) {
-					$notificationManager->createTrivialNotification($journalManager->getId(), NOTIFICATION_TYPE_ERROR, array('contents' => __('plugins.importexport.crossref.notification.failed')));
+				// If there are articles to be deposited
+				if (count($unregisteredArticles)) {
+					$this->_registerObjects($unregisteredArticles, 'article=>crossref-xml', $journal, 'articles');
 				}
 			}
 		}
@@ -96,7 +83,7 @@ class CrossrefInfoSender extends ScheduledTask {
 		$journals = array();
 		while($journal = $journalFactory->next()) {
 			$journalId = $journal->getId();
-			if (!$plugin->getSetting($journalId, 'username') || !$plugin->getSetting($journalId, 'password')) continue;
+			if (!$plugin->getSetting($journalId, 'username') || !$plugin->getSetting($journalId, 'password') || !$plugin->getSetting($journalId, 'automaticRegistration')) continue;
 
 			$doiPrefix = null;
 			$pubIdPlugins = PluginRegistry::loadCategory('pubIds', true, $journalId);
@@ -116,33 +103,6 @@ class CrossrefInfoSender extends ScheduledTask {
 	}
 
 	/**
-	 * Update the status and construct an array of the objects to be deposited
-	 * @param $unregisteredObjects array Array of all not fully registered objects
-	 * @param $journal Journal
-	 * @param $notify boolean
-	 * @return array Array of objects to be deposited
-	 */
-	function _getObjectsToBeDeposited($unregisteredObjects, $journal, &$notify) {
-		$plugin = $this->_plugin;
-		$objectsToBeDeposited = array();
-		foreach ($unregisteredObjects as $object) {
-			$plugin->updateDepositStatus($journal, $object);
-			// get the current object status
-			$currentStatus = $object->getData($plugin->getDepositStatusSettingName());
-			// deposit only not submitted objects
-			if (!$currentStatus) {
-				array_push($objectsToBeDeposited, $object);
-			}
-			// check if the new status after the update == failed to notify the users
-			$newStatus = $object->getData($plugin->getDepositStatusSettingName());
-			if (!$notify && $newStatus == CROSSREF_STATUS_FAILED && $currentStatus != CROSSREF_STATUS_FAILED) {
-				$notify = true;
-			}
-		}
-		return $objectsToBeDeposited;
-	}
-
-	/**
 	 * Register objects
 	 * @param $objects array
 	 * @param $filter string
@@ -153,18 +113,27 @@ class CrossrefInfoSender extends ScheduledTask {
 		$plugin = $this->_plugin;
 		import('lib.pkp.classes.file.FileManager');
 		$fileManager = new FileManager();
-		// export XML
-		$exportXml = $plugin->exportXML($objects, $filter, $journal);
-		// Write the XML to a file.
-		$exportFileName = $plugin->getExportFileName($plugin->getExportPath(), $objectsFileNamePart, $journal, '.xml');
-		$fileManager->writeFile($exportFileName, $exportXml);
-		// Deposit the XML file.
-		$result = $plugin->depositXML($objects, $journal, $exportFileName);
-		if ($result !== true) {
-			$this->_addLogEntry($result);
+		// The new Crossref deposit API expects one request per object.
+		// On contrary the export supports bulk/batch object export, thus
+		// also the filter expects an array of objects.
+		// Thus the foreach loop, but every object will be in an one item array for
+		// the export and filter to work.
+		foreach ($objects as $object) {
+			// export XML
+			$exportXml = $plugin->exportXML(array($object), $filter, $journal);
+			// Write the XML to a file.
+			// export file name example: crossref-20160723-160036-articles-1-1.xml
+			$objectsFileNamePart = $objectsFileNamePart . '-' . $object->getId();
+			$exportFileName = $this->getExportFileName($plugin->getExportPath(), $objectsFileNamePart, $journal, '.xml');
+			$fileManager->writeFile($exportFileName, $exportXml);
+			// Deposit the XML file.
+			$result = $plugin->depositXML($object, $journal, $exportFileName);
+			if ($result !== true) {
+				$this->_addLogEntry($result);
+			}
+			// Remove all temporary files.
+			$fileManager->deleteFile($exportFileName);
 		}
-		// Remove all temporary files.
-		$fileManager->deleteByPath($exportFileName);
 	}
 
 	/**

jardakotesovec/pkp-mono

ops/plugins/importexport/crossref/CrossrefInfoSender.inc.php
@@ -61,22 +61,9 @@ class CrossrefInfoSender extends ScheduledTask {
 			if ($doiPubIdPlugin->getSetting($journal->getId(), 'enableSubmissionDoi')) {
 				// Get unregistered articles
 				$unregisteredArticles = $plugin->getUnregisteredArticles($journal);
-				// Update the status and construct an array of the articles to be deposited
-				$articlesToBeDeposited = $this->_getObjectsToBeDeposited($unregisteredArticles, $journal, $notify);
-				// If there are articles to be deposited and we want automatic deposits
-				if (count($articlesToBeDeposited) && $plugin->getSetting($journal->getId(), 'automaticRegistration')) {
-					$this->_registerObjects($articlesToBeDeposited, 'article=>crossref-xml', $journal, 'articles');
-				}
-			}
-
-			// Notify journal managers if there is a new failed DOI status
-			if ($notify) {
-				$roleDao = DAORegistry::getDAO('RoleDAO');
-				$journalManagers = $roleDao->getUsersByRoleId(ROLE_ID_MANAGER, $journal->getId());
-				import('classes.notification.NotificationManager');
-				$notificationManager = new NotificationManager();
-				while ($journalManager = $journalManagers->next()) {
-					$notificationManager->createTrivialNotification($journalManager->getId(), NOTIFICATION_TYPE_ERROR, array('contents' => __('plugins.importexport.crossref.notification.failed')));
+				// If there are articles to be deposited
+				if (count($unregisteredArticles)) {
+					$this->_registerObjects($unregisteredArticles, 'article=>crossref-xml', $journal, 'articles');
 				}
 			}
 		}
@@ -96,7 +83,7 @@ class CrossrefInfoSender extends ScheduledTask {
 		$journals = array();
 		while($journal = $journalFactory->next()) {
 			$journalId = $journal->getId();
-			if (!$plugin->getSetting($journalId, 'username') || !$plugin->getSetting($journalId, 'password')) continue;
+			if (!$plugin->getSetting($journalId, 'username') || !$plugin->getSetting($journalId, 'password') || !$plugin->getSetting($journalId, 'automaticRegistration')) continue;
 
 			$doiPrefix = null;
 			$pubIdPlugins = PluginRegistry::loadCategory('pubIds', true, $journalId);
@@ -116,33 +103,7 @@ class CrossrefInfoSender extends ScheduledTask {
 	}
 
 	/**
-	 * Update the status and construct an array of the objects to be deposited
-	 * @param $unregisteredObjects array Array of all not fully registered objects
-	 * @param $journal Journal
-	 * @param $notify boolean
-	 * @return array Array of objects to be deposited
-	 */
-	function _getObjectsToBeDeposited($unregisteredObjects, $journal, &$notify) {
-		$plugin = $this->_plugin;
-		$objectsToBeDeposited = array();
-		foreach ($unregisteredObjects as $object) {
-			// get the current object status
-			$currentStatus = $object->getData($plugin->getDepositStatusSettingName());
-			$plugin->updateDepositStatus($journal, $object);
-			// check if the new status after the update == failed to notify the users
-			$newStatus = $object->getData($plugin->getDepositStatusSettingName());
-			// deposit only not submitted objects
-			if (!$newStatus) {
-				array_push($objectsToBeDeposited, $object);
-			}
-			if (!$notify && $newStatus == CROSSREF_STATUS_FAILED && $currentStatus != CROSSREF_STATUS_FAILED) {
-				$notify = true;
-			}
-		}
-		return $objectsToBeDeposited;
-	}
 
-	/**
 	 * Register objects
 	 * @param $objects array
 	 * @param $filter string
@@ -153,18 +114,27 @@ class CrossrefInfoSender extends ScheduledTask {
 		$plugin = $this->_plugin;
 		import('lib.pkp.classes.file.FileManager');
 		$fileManager = new FileManager();
-		// export XML
-		$exportXml = $plugin->exportXML($objects, $filter, $journal);
-		// Write the XML to a file.
-		$exportFileName = $plugin->getExportFileName($plugin->getExportPath(), $objectsFileNamePart, $journal, '.xml');
-		$fileManager->writeFile($exportFileName, $exportXml);
-		// Deposit the XML file.
-		$result = $plugin->depositXML($objects, $journal, $exportFileName);
-		if ($result !== true) {
-			$this->_addLogEntry($result);
+		// The new Crossref deposit API expects one request per object.
+		// On contrary the export supports bulk/batch object export, thus
+		// also the filter expects an array of objects.
+		// Thus the foreach loop, but every object will be in an one item array for
+		// the export and filter to work.
+		foreach ($objects as $object) {
+			// export XML
+			$exportXml = $plugin->exportXML(array($object), $filter, $journal);
+			// Write the XML to a file.
+			// export file name example: crossref-20160723-160036-articles-1-1.xml
+			$objectsFileNamePart = $objectsFileNamePart . '-' . $object->getId();
+			$exportFileName = $plugin->getExportFileName($plugin->getExportPath(), $objectsFileNamePart, $journal, '.xml');
+			$fileManager->writeFile($exportFileName, $exportXml);
+			// Deposit the XML file.
+			$result = $plugin->depositXML($object, $journal, $exportFileName);
+			if ($result !== true) {
+				$this->_addLogEntry($result);
+			}
+			// Remove all temporary files.
+			$fileManager->deleteByPath($exportFileName);
 		}
-		// Remove all temporary files.
-		$fileManager->deleteByPath($exportFileName);
 	}
 
 	/**