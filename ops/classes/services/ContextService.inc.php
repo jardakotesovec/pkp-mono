pkp/ojs

pkp/pkp-lib#4861 Migrates cover images to support versioning
- Combines coverImage and coverImageAltText settings into one
- Migrates settings to publication_settings table
- Updates article landing page to show correct publication cover image

classes/services/ContextService.inc.php
@@ -165,7 +165,7 @@ class ContextService extends \PKP\Services\PKPContextService {
 					if (!is_array($errors['journalThumbnail'])) {
 						$errors['journalThumbnail'] = [];
 					}
-					$errors['journalThumbnail'][$localeKey] = [__('manager.setup.noTemporaryFile')];
+					$errors['journalThumbnail'][$localeKey] = [__('common.noTemporaryFile')];
 				}
 			}
 		}

jardakotesovec/pkp-mono

ops/classes/services/ContextService.inc.php
@@ -165,7 +165,7 @@ class ContextService extends \PKP\Services\PKPContextService {
 					if (!is_array($errors['serverThumbnail'])) {
 						$errors['serverThumbnail'] = [];
 					}
-					$errors['serverThumbnail'][$localeKey] = [__('manager.setup.noTemporaryFile')];
+					$errors['serverThumbnail'][$localeKey] = [__('common.noTemporaryFile')];
 				}
 			}
 		}