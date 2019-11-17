pkp/ojs

pkp/pkp-lib#4593 Fix incorrect access indications in category listing

templates/frontend/pages/catalogCategory.tpl
@@ -71,7 +71,7 @@
 		<ul class="cmp_article_list articles">
 			{foreach from=$publishedSubmissions item=article}
 				<li>
-					{include file="frontend/objects/article_summary.tpl"}
+					{include file="frontend/objects/article_summary.tpl" article=$article hideGalleys=true}
 				</li>
 			{/foreach}
 		</ul>

jardakotesovec/pkp-mono

ops/templates/frontend/pages/catalogCategory.tpl
@@ -71,7 +71,7 @@
 		<ul class="cmp_article_list articles">
 			{foreach from=$publishedSubmissions item=preprint}
 				<li>
-					{include file="frontend/objects/preprint_summary.tpl"}
+					{include file="frontend/objects/preprint_summary.tpl" preprint=$preprint hideGalleys=true}
 				</li>
 			{/foreach}
 		</ul>