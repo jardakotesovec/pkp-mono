henriqueramos/ojs

pkp/pkp-lib#7125 Replacing SubmissionFileDAO to new Repository pattern

classes/plugins/PubObjectsExportPlugin.inc.php
@@ -862,7 +862,7 @@ abstract class PubObjectsExportPlugin extends ImportExportPlugin
             Repo::publication()->dao,
             Repo::submission()->dao,
             Application::getRepresentationDAO(),
-            DAORegistry::getDAO('SubmissionFileDAO'),
+            Repo::submissionFiles()->dao,
             DAORegistry::getDAO('IssueDAO'),
         ];
     }

jardakotesovec/pkp-mono

ojs/classes/plugins/PubObjectsExportPlugin.inc.php
@@ -863,8 +863,8 @@ abstract class PubObjectsExportPlugin extends ImportExportPlugin
             Repo::publication()->dao,
             Repo::submission()->dao,
             Application::getRepresentationDAO(),
-            DAORegistry::getDAO('SubmissionFileDAO'),
-            Repo::issue()->dao
+            Repo::issue()->dao,
+            Repo::submissionFiles()->dao
         ];
     }
 }