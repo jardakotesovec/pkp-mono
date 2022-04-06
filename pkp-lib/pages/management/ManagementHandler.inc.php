jardakotesovec/pkp-mono

commit 668241811b093765a897cfe8ae89927793eccbd4

pkp/pkp-lib#6328 Removed locales.xml and unused dtd and xml files - Update code to replace the locales.xml (added detection of text direction + completeness calculation + integrated ISO Alpha 2/3) - Implemented an "IsoCodes" translation driver to replace the default driver, the GettextExtensionDriver doesn't work very well and the SymfonyTranslationDriver has a memory/performance issue.

pkp-lib/pages/management/ManagementHandler.inc.php
@@ -22,7 +22,6 @@ use PKP\components\forms\context\PKPNotifyUsersForm;
 use PKP\config\Config;
 use PKP\core\PKPApplication;
 use PKP\db\DAORegistry;
-use PKP\facades\Locale;
 use PKP\security\authorization\ContextAccessPolicy;
 use PKP\security\Role;
 use PKP\site\VersionCheck;
@@ -111,10 +110,8 @@ class ManagementHandler extends Handler
         $apiUrl = $dispatcher->url($request, PKPApplication::ROUTE_API, $context->getPath(), 'contexts/' . $context->getId());
         $publicFileApiUrl = $dispatcher->url($request, PKPApplication::ROUTE_API, $context->getPath(), '_uploadPublicFile');
 
-        $supportedFormLocales = $context->getSupportedFormLocales();
-        $locales = array_map(function ($localeKey) {
-            return ['key' => $localeKey, 'label' => Locale::getLocaleMetadata($localeKey)->name];
-        }, $supportedFormLocales);
+        $locales = $context->getSupportedSubmissionLocaleNames();
+        $locales = array_map(fn (string $locale, string $name) => ['key' => $locale, 'label' => $name], array_keys($locales), $locales);
 
         $contactForm = new PKP\components\forms\context\PKPContactForm($apiUrl, $locales, $context);
         $mastheadForm = new APP\components\forms\context\MastheadForm($apiUrl, $locales, $context, $publicFileApiUrl);
@@ -175,10 +172,8 @@ class ManagementHandler extends Handler
         $publicFileManager = new PublicFileManager();
         $baseUrl = $request->getBaseUrl() . '/' . $publicFileManager->getContextFilesPath($context->getId());
 
-        $supportedFormLocales = $context->getSupportedFormLocales();
-        $locales = array_map(function ($localeKey) {
-            return ['key' => $localeKey, 'label' => Locale::getLocaleMetadata($localeKey)->name];
-        }, $supportedFormLocales);
+        $locales = $context->getSupportedSubmissionLocaleNames();
+        $locales = array_map(fn (string $locale, string $name) => ['key' => $locale, 'label' => $name], array_keys($locales), $locales);
 
         $announcementSettingsForm = new \PKP\components\forms\context\PKPAnnouncementSettingsForm($contextApiUrl, $locales, $context);
         $appearanceAdvancedForm = new \APP\components\forms\context\AppearanceAdvancedForm($contextApiUrl, $locales, $context, $baseUrl, $temporaryFileApiUrl, $publicFileApiUrl);
@@ -231,10 +226,8 @@ class ManagementHandler extends Handler
         $contextApiUrl = $dispatcher->url($request, PKPApplication::ROUTE_API, $context->getPath(), 'contexts/' . $context->getId());
         $emailTemplatesApiUrl = $dispatcher->url($request, PKPApplication::ROUTE_API, $context->getPath(), 'emailTemplates');
 
-        $supportedFormLocales = $context->getSupportedFormLocales();
-        $locales = array_map(function ($localeKey) {
-            return ['key' => $localeKey, 'label' => Locale::getLocaleMetadata($localeKey)->name];
-        }, $supportedFormLocales);
+        $locales = $context->getSupportedSubmissionLocaleNames();
+        $locales = array_map(fn (string $locale, string $name) => ['key' => $locale, 'label' => $name], array_keys($locales), $locales);
 
         $authorGuidelinesForm = new \PKP\components\forms\context\PKPAuthorGuidelinesForm($contextApiUrl, $locales, $context);
         $metadataSettingsForm = new \APP\components\forms\context\MetadataSettingsForm($contextApiUrl, $context);
@@ -290,10 +283,8 @@ class ManagementHandler extends Handler
         $sitemapUrl = $router->url($request, $context->getPath(), 'sitemap');
         $paymentsUrl = $dispatcher->url($request, PKPApplication::ROUTE_API, $context->getPath(), '_payments');
 
-        $supportedFormLocales = $context->getSupportedFormLocales();
-        $locales = array_map(function ($localeKey) {
-            return ['key' => $localeKey, 'label' => Locale::getLocaleMetadata($localeKey)->name];
-        }, $supportedFormLocales);
+        $locales = $context->getSupportedSubmissionLocaleNames();
+        $locales = array_map(fn (string $locale, string $name) => ['key' => $locale, 'label' => $name], array_keys($locales), $locales);
 
         $licenseForm = new \APP\components\forms\context\LicenseForm($apiUrl, $locales, $context);
         $doiSetupSettingsForm = new DoiSetupSettingsForm($apiUrl, $locales, $context);
@@ -329,11 +320,10 @@ class ManagementHandler extends Handler
         $this->setupTemplate($request);
 
         $apiUrl = $request->getDispatcher()->url($request, PKPApplication::ROUTE_API, $request->getContext()->getPath(), 'announcements');
+        $context = $request->getContext();
 
-        $supportedFormLocales = $request->getContext()->getSupportedFormLocales();
-        $locales = array_map(function ($localeKey) {
-            return ['key' => $localeKey, 'label' => Locale::getLocaleMetadata($localeKey)->name];
-        }, $supportedFormLocales);
+        $locales = $context->getSupportedSubmissionLocaleNames();
+        $locales = array_map(fn (string $locale, string $name) => ['key' => $locale, 'label' => $name], array_keys($locales), $locales);
 
         $announcementForm = new \PKP\components\forms\announcement\PKPAnnouncementForm($apiUrl, $locales, $request->getContext());
 

pkp-lib/classes/category/Repository.inc.php
@@ -16,7 +16,6 @@ namespace PKP\category;
 use APP\core\Request;
 use Illuminate\Support\Collection;
 use Illuminate\Support\LazyCollection;
-use PKP\facades\Locale;
 use PKP\plugins\HookRegistry;
 use PKP\services\PKPSchemaService;
 use PKP\validation\ValidatorFactory;

pkp-lib/classes/components/listPanels/PKPContributorsListPanel.inc.php
@@ -14,8 +14,6 @@
 
 namespace PKP\components\listPanels;
 
-use PKP\facades\Locale;
-
 class PKPContributorsListPanel extends ListPanel
 {
     /** @param \PKP\components\forms\publication\PKPContributorForm Form for adding or editing a contributor */

pkp-lib/classes/context/Context.inc.php
@@ -16,10 +16,10 @@
 namespace PKP\context;
 
 use APP\core\Application;
-use PKP\facades\Locale;
-
-use APP\plugins\IDoiRegistrationAgency;
+use Illuminate\Support\Arr;
 use PKP\config\Config;
+use PKP\facades\Locale;
+use APP\plugins\IDoiRegistrationAgency;
 use PKP\plugins\Plugin;
 use PKP\plugins\PluginRegistry;
 use PKP\statistics\PKPStatisticsHelper;
@@ -304,13 +304,9 @@ abstract class Context extends \PKP\core\DataObject
      */
     public function getLocalizedFavicon()
     {
-        $faviconArray = $this->getData('favicon');
-        foreach ([Locale::getLocale(), Locale::getPrimaryLocale()] as $locale) {
-            if (isset($faviconArray[$locale])) {
-                return $faviconArray[$locale];
-            }
-        }
-        return null;
+        $favicons = $this->getData('favicon');
+        $locale = Arr::first([Locale::getLocale(), Locale::getPrimaryLocale()], fn (string $locale) => isset($favicons[$locale]));
+        return $favicons[$locale] ?? null;
     }
 
     /**
@@ -331,21 +327,10 @@ abstract class Context extends \PKP\core\DataObject
      */
     public function getSupportedFormLocaleNames()
     {
-        $supportedLocales = & $this->getData('supportedFormLocaleNames');
-
-        if (!isset($supportedLocales)) {
-            $supportedLocales = [];
-            $locales = $this->getSupportedFormLocales();
-            if (!isset($locales) || !is_array($locales)) {
-                $locales = [];
-            }
-
-            foreach ($locales as $localeKey) {
-                $supportedLocales[$localeKey] = Locale::getLocaleMetadata($localeKey)->name;
-            }
-        }
-
-        return $supportedLocales;
+        return $this->getData('supportedFormLocaleNames') ?? array_map(
+            fn (string $locale) => Locale::getMetadata($locale)->getDisplayName(),
+            array_combine($locales = $this->getSupportedFormLocales(), $locales)
+        );
     }
 
     /**
@@ -367,21 +352,10 @@ abstract class Context extends \PKP\core\DataObject
      */
     public function getSupportedSubmissionLocaleNames()
     {
-        $supportedLocales = & $this->getData('supportedSubmissionLocaleNames');
-
-        if (!isset($supportedLocales)) {
-            $supportedLocales = [];
-            $locales = $this->getSupportedSubmissionLocales();
-            if (!isset($locales) || !is_array($locales)) {
-                $locales = [];
-            }
-
-            foreach ($locales as $localeKey) {
-                $supportedLocales[$localeKey] = Locale::getLocaleMetadata($localeKey)->name;
-            }
-        }
-
-        return $supportedLocales;
+        return $this->getData('supportedSubmissionLocaleNames') ?? array_map(
+            fn (string $locale) => Locale::getMetadata($locale)->getDisplayName(),
+            array_combine($locales = $this->getSupportedSubmissionLocales(), $locales)
+        );
     }
 
     /**
@@ -402,21 +376,10 @@ abstract class Context extends \PKP\core\DataObject
      */
     public function getSupportedLocaleNames()
     {
-        $supportedLocales = & $this->getData('supportedLocaleNames');
-
-        if (!isset($supportedLocales)) {
-            $supportedLocales = [];
-            $locales = $this->getSupportedLocales();
-            if (!isset($locales) || !is_array($locales)) {
-                $locales = [];
-            }
-
-            foreach ($locales as $localeKey) {
-                $supportedLocales[$localeKey] = Locale::getLocaleMetadata($localeKey)->name;
-            }
-        }
-
-        return $supportedLocales;
+        return $this->getData('supportedLocaleNames') ?? array_map(
+            fn (string $locale) => Locale::getMetadata($locale)->getDisplayName(),
+            array_combine($locales = $this->getSupportedLocales(), $locales)
+        );
     }
 
     /**
@@ -448,15 +411,7 @@ abstract class Context extends \PKP\core\DataObject
      */
     public function getLocalizedDateFormatShort($locale = null)
     {
-        if (is_null($locale)) {
-            $locale = Locale::getLocale();
-        }
-        $localizedData = $this->getData('dateFormatShort', $locale);
-        if (empty($localizedData)) {
-            $localizedData = Config::getVar('general', 'date_format_short');
-        }
-
-        return $localizedData;
+        return $this->getData('dateFormatShort', $locale ?? Locale::getLocale()) ?: Config::getVar('general', 'date_format_short');
     }
 
     /**
@@ -468,15 +423,7 @@ abstract class Context extends \PKP\core\DataObject
      */
     public function getLocalizedDateFormatLong($locale = null)
     {
-        if (is_null($locale)) {
-            $locale = Locale::getLocale();
-        }
-        $localizedData = $this->getData('dateFormatLong', $locale);
-        if (empty($localizedData)) {
-            $localizedData = Config::getVar('general', 'date_format_long');
-        }
-
-        return $localizedData;
+        return $this->getData('dateFormatLong', $locale ?? Locale::getLocale()) ?: Config::getVar('general', 'date_format_long');
     }
 
     /**
@@ -488,15 +435,7 @@ abstract class Context extends \PKP\core\DataObject
      */
     public function getLocalizedTimeFormat($locale = null)
     {
-        if (is_null($locale)) {
-            $locale = Locale::getLocale();
-        }
-        $localizedData = $this->getData('timeFormat', $locale);
-        if (empty($localizedData)) {
-            $localizedData = Config::getVar('general', 'time_format');
-        }
-
-        return $localizedData;
+        return $this->getData('timeFormat', $locale ?? Locale::getLocale()) ?: Config::getVar('general', 'time_format');
     }
 
     /**
@@ -508,15 +447,7 @@ abstract class Context extends \PKP\core\DataObject
      */
     public function getLocalizedDateTimeFormatShort($locale = null)
     {
-        if (is_null($locale)) {
-            $locale = Locale::getLocale();
-        }
-        $localizedData = $this->getData('datetimeFormatShort', $locale);
-        if (empty($localizedData)) {
-            $localizedData = Config::getVar('general', 'datetime_format_short');
-        }
-
-        return $localizedData;
+        return $this->getData('datetimeFormatShort', $locale ?? Locale::getLocale()) ?: Config::getVar('general', 'datetime_format_short');
     }
 
     /**
@@ -528,15 +459,7 @@ abstract class Context extends \PKP\core\DataObject
      */
     public function getLocalizedDateTimeFormatLong($locale = null)
     {
-        if (is_null($locale)) {
-            $locale = Locale::getLocale();
-        }
-        $localizedData = $this->getData('datetimeFormatLong', $locale);
-        if (empty($localizedData)) {
-            $localizedData = Config::getVar('general', 'datetime_format_long');
-        }
-
-        return $localizedData;
+        return $this->getData('datetimeFormatLong', $locale ?? Locale::getLocale()) ?: Config::getVar('general', 'datetime_format_long');
     }
 
     /**

pkp-lib/classes/core/PKPApplication.inc.php
@@ -23,18 +23,16 @@ use PKP\facades\Locale;
 use APP\statistics\StatisticsHelper;
 use DateTime;
 use DateTimeZone;
-use DomainException;
 use Exception;
 use GuzzleHttp\Client;
-use Illuminate\Support\Facades\Cache;
+use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Database\MySqlConnection;
 use Illuminate\Support\Facades\DB;
 use PKP\config\Config;
 use PKP\db\DAORegistry;
 use PKP\plugins\PluginRegistry;
 use PKP\security\Role;
 use PKP\session\SessionManager;
-use PKP\site\VersionCheck;
-use PKP\site\VersionDAO;
 use PKP\statistics\PKPStatisticsHelper;
 
 interface iPKPApplicationInfoProvider
@@ -209,14 +207,11 @@ abstract class PKPApplication implements iPKPApplicationInfoProvider
         $microTime = Core::microtime();
         Registry::set('system.debug.startTime', $microTime);
 
-        $notes = [];
-        Registry::set('system.debug.notes', $notes);
-
         $this->initializeLaravelContainer();
         PKPString::initialize();
 
         // Load default locale files
-        Locale::registerFolder(BASE_SYS_DIR . '/lib/pkp/locale');
+        Locale::registerPath(BASE_SYS_DIR . '/lib/pkp/locale');
     }
 
     /**
@@ -236,16 +231,10 @@ abstract class PKPApplication implements iPKPApplicationInfoProvider
         $laravelContainer = new PKPContainer();
         $laravelContainer->registerConfiguredProviders();
 
-        // Add the application version as a cache invalidator (avoid unstable deserializations due to structural changes)
-        if (Cache::getStore() instanceof \ElcoBvg\Opcache\Store) {
-            Cache::setSubDirectory(preg_replace('/[^\da-z.]/i', '-', VersionCheck::getCurrentCodeVersion()->getVersionString()));
-        }
         $this->initializeTimeZone();
 
         if (Config::getVar('database', 'debug')) {
-            DB::listen(function ($query) {
-                error_log("Database query\n{$query->sql}\n" . json_encode($query->bindings));
-            });
+            DB::listen(fn(QueryExecuted $query) => error_log("Database query\n{$query->sql}\n" . json_encode($query->bindings)));
         }
     }
 
@@ -277,18 +266,9 @@ abstract class PKPApplication implements iPKPApplicationInfoProvider
         if (Application::isInstalled()) {
             // Retrieve the current offset
             $offset = (new DateTime())->format('P');
-
-            // Setup the database time zone
-            switch (get_class(DB::connection())) {
-                case \Illuminate\Database\MySqlConnection::class:
-                    $statement = "SET time_zone = '$offset'";
-                    break;
-                case \Illuminate\Database\PostgresConnection::class:
-                    $statement = "SET TIME ZONE INTERVAL '$offset' HOUR TO MINUTE";
-                    break;
-                default:
-                    throw new DomainException('Unrecognized database');
-            }
+            $statement = DB::connection() instanceof MySqlConnection
+                ? "SET time_zone = '${offset}'"
+                : "SET TIME ZONE INTERVAL '${offset}' HOUR TO MINUTE";
             DB::statement($statement);
         }
     }
@@ -323,6 +303,7 @@ abstract class PKPApplication implements iPKPApplicationInfoProvider
         $application = Application::get();
         $userAgent = $application->getName() . '/';
         if (static::isInstalled() && !static::isUpgrading()) {
+            /** @var \PKP\site\VersionDAO */
             $versionDao = DAORegistry::getDAO('VersionDAO');
             $currentVersion = $versionDao->getCurrentVersion();
             $userAgent .= $currentVersion->getVersionString();
@@ -473,7 +454,7 @@ abstract class PKPApplication implements iPKPApplicationInfoProvider
                 $settingContext = array_combine($this->getContextList(), $settingContext);
             }
 
-            $versionDao = DAORegistry::getDAO('VersionDAO'); /** @var VersionDAO $versionDao */
+            $versionDao = DAORegistry::getDAO('VersionDAO'); /** @var \PKP\site\VersionDAO $versionDao */
             $this->enabledProducts[$mainContextId] = $versionDao->getCurrentProducts($settingContext);
         }
 

pkp-lib/classes/core/PKPContainer.inc.php
@@ -29,11 +29,8 @@ use Illuminate\Queue\Failed\DatabaseFailedJobProvider;
 use Illuminate\Support\Facades\Facade;
 use PKP\config\Config;
 use PKP\Domains\Jobs\Providers\JobServiceProvider;
-use PKP\facades\Locale;
 use PKP\i18n\LocaleServiceProvider;
 use PKP\Support\ProxyParser;
-use Sokil\IsoCodes\IsoCodesFactory;
-use Sokil\IsoCodes\TranslationDriver\GettextExtensionDriver;
 
 use Throwable;
 
@@ -93,13 +90,6 @@ class PKPContainer extends Container
             KernelContract::class,
             Kernel::class
         );
-
-        // This singleton is necessary to keep user selected language across the application
-        $this->singleton(IsoCodesFactory::class, function (self $container, array $params): IsoCodesFactory {
-            $driver = new GettextExtensionDriver();
-            $driver->setLocale($params['locale'] ?? Locale::getLocale());
-            return new IsoCodesFactory(null, $driver);
-        });
 
         $this->singleton(
             'queue.failer',
@@ -258,7 +248,7 @@ class PKPContainer extends Container
             'stores' => [
                 'opcache' => [
                     'driver' => 'opcache',
-                    'path' => Core::getBaseDir() . '/cache'
+                    'path' => Core::getBaseDir() . '/cache/opcache'
                 ]
             ]
         ];

pkp-lib/classes/emailTemplate/DAO.inc.php
@@ -323,8 +323,6 @@ class DAO extends EntityDAO
                         ->where('locale', $locale)
                         ->delete();
 
-
-
                     $previous = Locale::getMissingKeyHandler();
                     Locale::setMissingKeyHandler(fn (string $key): string => '');
                     $translatedSubject = __($subject, [], $locale);
@@ -376,9 +374,7 @@ class DAO extends EntityDAO
 
             // Translate variable contents
             foreach ([&$subject, &$body, &$description] as &$var) {
-                $var = preg_replace_callback('{{translate key="([^"]+)"}}', function ($matches) use($locale) {
-                    return __($matches[1], [], $locale);
-                }, $var);
+                $var = preg_replace_callback('{{translate key="([^"]+)"}}', fn($matches) => __($matches[1], [], $locale), $var);
             }
 
             if ($emailKey && $emailKey != $emailNode->getAttribute('key')) {

pkp-lib/classes/emailTemplate/Repository.inc.php
@@ -18,7 +18,6 @@ use Illuminate\Support\LazyCollection;
 use PKP\core\PKPRequest;
 use PKP\plugins\HookRegistry;
 use PKP\services\PKPSchemaService;
-use PKP\facades\Locale;
 use PKP\validation\ValidatorFactory;
 use PKP\facades\Repo;
 

commit 852b7d763c9fce2dff522d4c5158d61e61e669d7

Fixed typo introduced in 668241811b093765a897cfe8ae89927793eccbd4

pkp-lib/pages/management/ManagementHandler.inc.php
@@ -110,7 +110,7 @@ class ManagementHandler extends Handler
         $apiUrl = $dispatcher->url($request, PKPApplication::ROUTE_API, $context->getPath(), 'contexts/' . $context->getId());
         $publicFileApiUrl = $dispatcher->url($request, PKPApplication::ROUTE_API, $context->getPath(), '_uploadPublicFile');
 
-        $locales = $context->getSupportedSubmissionLocaleNames();
+        $locales = $context->getSupportedFormLocaleNames();
         $locales = array_map(fn (string $locale, string $name) => ['key' => $locale, 'label' => $name], array_keys($locales), $locales);
 
         $contactForm = new PKP\components\forms\context\PKPContactForm($apiUrl, $locales, $context);
@@ -172,7 +172,7 @@ class ManagementHandler extends Handler
         $publicFileManager = new PublicFileManager();
         $baseUrl = $request->getBaseUrl() . '/' . $publicFileManager->getContextFilesPath($context->getId());
 
-        $locales = $context->getSupportedSubmissionLocaleNames();
+        $locales = $context->getSupportedFormLocaleNames();
         $locales = array_map(fn (string $locale, string $name) => ['key' => $locale, 'label' => $name], array_keys($locales), $locales);
 
         $announcementSettingsForm = new \PKP\components\forms\context\PKPAnnouncementSettingsForm($contextApiUrl, $locales, $context);
@@ -226,7 +226,7 @@ class ManagementHandler extends Handler
         $contextApiUrl = $dispatcher->url($request, PKPApplication::ROUTE_API, $context->getPath(), 'contexts/' . $context->getId());
         $emailTemplatesApiUrl = $dispatcher->url($request, PKPApplication::ROUTE_API, $context->getPath(), 'emailTemplates');
 
-        $locales = $context->getSupportedSubmissionLocaleNames();
+        $locales = $context->getSupportedFormLocaleNames();
         $locales = array_map(fn (string $locale, string $name) => ['key' => $locale, 'label' => $name], array_keys($locales), $locales);
 
         $authorGuidelinesForm = new \PKP\components\forms\context\PKPAuthorGuidelinesForm($contextApiUrl, $locales, $context);
@@ -283,7 +283,7 @@ class ManagementHandler extends Handler
         $sitemapUrl = $router->url($request, $context->getPath(), 'sitemap');
         $paymentsUrl = $dispatcher->url($request, PKPApplication::ROUTE_API, $context->getPath(), '_payments');
 
-        $locales = $context->getSupportedSubmissionLocaleNames();
+        $locales = $context->getSupportedFormLocaleNames();
         $locales = array_map(fn (string $locale, string $name) => ['key' => $locale, 'label' => $name], array_keys($locales), $locales);
 
         $licenseForm = new \APP\components\forms\context\LicenseForm($apiUrl, $locales, $context);
@@ -322,7 +322,7 @@ class ManagementHandler extends Handler
         $apiUrl = $request->getDispatcher()->url($request, PKPApplication::ROUTE_API, $request->getContext()->getPath(), 'announcements');
         $context = $request->getContext();
 
-        $locales = $context->getSupportedSubmissionLocaleNames();
+        $locales = $context->getSupportedFormLocaleNames();
         $locales = array_map(fn (string $locale, string $name) => ['key' => $locale, 'label' => $name], array_keys($locales), $locales);
 
         $announcementForm = new \PKP\components\forms\announcement\PKPAnnouncementForm($apiUrl, $locales, $request->getContext());

pkp-lib/plugins/importexport/native/filter/PKPPublicationNativeXmlFilter.inc.php
@@ -218,7 +218,7 @@ class PKPPublicationNativeXmlFilter extends NativeExportFilter
 
         // add controlled vocabularies
         // get the supported locale keys
-        $supportedLocales = array_keys($deployment->getContext()->getSupportedFormLocaleNames());
+        $supportedLocales = $deployment->getContext()->getSupportedFormLocales();
         $controlledVocabulariesMapping = $this->_getControlledVocabulariesMappings();
         foreach ($controlledVocabulariesMapping as $controlledVocabulariesNodeName => $mappings) {
             $dao = DAORegistry::getDAO($mappings[0]);